771f5de362508b954b7612acec5ef572315538b6
Gabriel Poca <mail@gabrielpoca.com>
2022-02-24 16:33:35 +0000

    :sunrise:

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import fastify from "fastify";
+
+const server = fastify({
+  logger: {
+    prettyPrint:
+      process.env.NODE_ENV !== "development"
+        ? {
+            translateTime: "HH:MM:ss Z",
+            ignore: "pid,hostname",
+          }
+        : false,
+  },
+});
+
+server.get("/ping", async (_request, _reply) => {
+  return "pong\n";
+});
+
+server.listen(process.env.PORT || 8080, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+
+  server.log.debug(`Server listening at ${address}`);
+});
