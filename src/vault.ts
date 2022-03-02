9f08dd63dfd8ef1af8ecd4d9bdce0c5197568f82
Martin Stuessy <contact@martinstuessy.com>
2022-03-02 12:35:08 +0000

    make vault.ts logic more terse and do not expose unecessary functions/variables

--- src/vault.ts
+++ src/vault.ts
@@ -9,62 +9,49 @@ const addresses: {
   [network: string]: { [contractType: string]: { [token: string]: string } };
 } = addressesJson;
 
-export const provider: providers.WebSocketProvider =
-  new providers.WebSocketProvider(
-    process.env.RPC_URL || 'http://127.0.0.1:8545',
-  );
-
-export const vaults: Contracts = _.reduce(
-  addresses.ropsten.vault,
-  (memo: Contracts, address, vault) => {
-    memo[vault] = new Contract(address, vaultABI, provider);
-    return memo;
-  },
-  {},
-);
-
-export function generateContractCalls(
-  contracts: Contracts,
-  call: string,
-): Promise<any>[] {
+function contractCalls(contracts: Contracts, call: string): Promise<any>[] {
   return _.reduce(
     contracts,
     (memo: Promise<any>[], contract) => {
-      memo.push(contract[call]);
+      memo.push(contract[call]());
       return memo;
     },
     [],
   );
 }
 
-export async function vaultPerformance(
-  totalSharesCalls: Promise<BigNumber>[],
-  totalUnderlyingMinusSponsoredCalls: Promise<BigNumber>[],
-): Promise<BigNumber[]> {
-  if (totalSharesCalls.length !== totalUnderlyingMinusSponsoredCalls.length) {
-    throw 'mismatch between totalSharesCalls and totalUnderlyingMinusSponsoredCalls';
-  }
+const provider: providers.WebSocketProvider = new providers.WebSocketProvider(
+  process.env.RPC_URL || 'http://127.0.0.1:8545',
+);
 
+export const vaults: Contracts = _.reduce(
+  addresses.ropsten.vault,
+  (memo: Contracts, address: string, vault: string) => {
+    memo[vault] = new Contract(address, vaultABI, provider);
+    return memo;
+  },
+  {},
+);
+
+export async function vaultPerformance(): Promise<BigNumber[]> {
   const response = await Promise.allSettled([
-    ...totalSharesCalls,
-    ...totalUnderlyingMinusSponsoredCalls,
-  ]).then((values) => {
-    return values.map((values) => {
-      // When rejected, fall back to BigNumber 0.
-      if (values.status === 'rejected') {
-        return BigNumber.from('0');
-      }
+    ...contractCalls(vaults, 'totalShares'),
+    ...contractCalls(vaults, 'totalUnderlyingMinusSponsored'),
+  ]);
 
-      return values.value;
-    });
+  const metrics = response.map((values) => {
+    // When rejected, fall back to BigNumber 0.
+    if (values.status === 'rejected') {
+      return BigNumber.from('0');
+    }
+
+    return values.value;
   });
 
-  const vaultTotalShares = response.slice(0, totalSharesCalls.length);
-  const vaultTotalUnderlyingMinusSponsored = response.slice(
-    -totalSharesCalls.length,
+  const vaultTotalShares = metrics.slice(0, metrics.length);
+  const vaultTotalUnderlyingMinusSponsored = metrics.slice(-metrics.length);
+
+  return vaultTotalShares.map((totalShare: BigNumber, i: number) =>
+    totalShare.div(vaultTotalUnderlyingMinusSponsored[i]),
   );
-
-  return vaultTotalShares.map((totalShare: BigNumber, i: number) => {
-    return totalShare.div(vaultTotalUnderlyingMinusSponsored[i]);
-  });
 }
