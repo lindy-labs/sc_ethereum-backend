630734765f24be22f33455ae71d2abb62c5e91ea
0xrin1 <0xrin1@protonmail.com>
2022-03-14 17:16:46 +0000

    initVaultMetricRepository

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
-import { init as initVaultMetricRep } from './repositories/vaultMetric';
+import { init as initVaultMetricRepository } from './repositories/vaultMetric';
 
 export * from './entities/vaultMetric';
 
 export function initRepos() {
-  initVaultMetricRep();
+  initVaultMetricRepository();
 }
