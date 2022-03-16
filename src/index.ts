e7cf2e582512a2b2a3125b4d26feda7df0b07050
Gabriel Poca <mail@gabrielpoca.com>
2022-03-16 11:52:13 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Connection } from 'typeorm';
 
 import { server } from './server';
 import getConnection from './db/getConnection';
-import { initSchedule } from './scheduler';
+// import { initSchedule } from './scheduler';
 import { initRepos } from './db';
 
 let connection: Connection;
@@ -12,33 +12,42 @@ server.get('/ping', async (_request, _reply) => {
   return 'pong\n';
 });
 
-getConnection().then(async (newConnection) => {
-  connection = newConnection;
+getConnection()
+  .then(async (newConnection) => {
+    connection = newConnection;
 
-  server.register(typeorm, {
-    connection: newConnection,
-  });
+    server.register(typeorm, {
+      connection: newConnection!,
+    });
 
-  server.listen(process.env.PORT || 8080, (err, address) => {
-    if (err) {
-      server.log.error(err);
-      process.exit(1);
-    }
+    server.listen(process.env.PORT || 8080, (err, address) => {
+      if (err) {
+        server.log.error(err);
+        process.exit(1);
+      }
 
-    server.log.debug(`Server listening at ${address}`);
+      server.log.debug(`Server listening at ${address}`);
 
-    initRepos();
+      initRepos();
+      // initSchedule();
+    });
+  })
+  .catch(console.error);
 
-    initSchedule();
-  });
-});
-
-process.on('uncaughtException', (err) => {
-  console.error('uncaught error', err);
+function handleExit(code?: number) {
+  console.log('closing');
 
   Promise.all([connection.close(), server.close()]).finally(() =>
-    process.exit(1),
+    process.exit(code || 0),
   );
 
   setTimeout(() => process.abort(), 1000).unref();
+}
+
+process.on('SIGINT', handleExit);
+process.on('SIGQUIT', handleExit);
+process.on('SIGTERM', handleExit);
+process.on('uncaughtException', (err) => {
+  console.error('uncaught error', err);
+  handleExit(1);
 });
