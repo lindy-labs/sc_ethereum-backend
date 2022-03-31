417f9e040fc55cbf575fc0f35528635029a242e8
joaop21 <jpsilva9898@gmail.com>
2022-03-31 17:24:32 +0100

    Stop monitoring on close

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ function handleExit(code?: number) {
     connection?.close(),
     API.server.close(),
     Scheduler.stop(),
+    Monitoring.stop(),
   ]).finally(() => process.exit(code || 0));
 
   setTimeout(() => process.abort(), 1000).unref();
