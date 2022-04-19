cdbea5e7f30a7d27a014c48977fa9e92699e09b6
joaop21 <jpsilva9898@gmail.com>
2022-04-19 15:20:36 +0100

    Add server entry file and scheduler

---
+++ src/scheduler/server.ts
@@ -0,0 +1,72 @@
+import { Job, Queue, QueueScheduler, Worker } from 'bullmq';
+import Redis from 'ioredis';
+import * as Monitoring from '../monitoring';
+
+import logger from '../logger';
+import refreshOrganizations from '../jobs/refreshOrganizations';
+
+const SCHEDULER_QUEUE = 'ServerSchedulerQueue';
+
+const redisOptions: Redis.RedisOptions = {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+};
+
+const connection = new Redis(
+  process.env.REDIS_URL || 'redis://127.0.0.1:6379',
+  redisOptions,
+);
+
+const options = {
+  connection,
+};
+
+const scheduler = new QueueScheduler(SCHEDULER_QUEUE, options);
+const schedulerQueue = new Queue(SCHEDULER_QUEUE, options);
+
+const schedulerWorker = new Worker(
+  SCHEDULER_QUEUE,
+  async (job: Job) => {
+    switch (job.name) {
+      case 'refreshOrganizations':
+        await refreshOrganizations(job.data);
+        break;
+    }
+  },
+
+  options,
+);
+
+schedulerQueue.add('refreshOrganizations', null, {
+  repeat: {
+    every: 1000 * 60 * 60 * 4, // every 4 hours
+  },
+  jobId: 'refreshOrganizations',
+});
+
+schedulerWorker.on('completed', (job: Job, err: Error) => {
+  logger.info(`${job.id} has been completed!`);
+});
+
+schedulerWorker.on('failed', (job: Job, err: Error) => {
+  logger.error(`${job.id} has failed with ${err.message}`);
+  Monitoring.captureException(err);
+});
+
+export async function start() {
+  schedulerQueue.add(
+    'refreshOrganizations',
+    { force: true },
+    {
+      jobId: 'refreshOrganizations',
+    },
+  );
+}
+
+export async function stop() {
+  return await Promise.all([
+    scheduler.close(),
+    schedulerQueue.close(),
+    schedulerWorker.close(),
+  ]);
+}
