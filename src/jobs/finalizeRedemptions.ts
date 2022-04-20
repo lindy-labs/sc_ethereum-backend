7fa1b08e7acf51e2a68e0b5186105f21e609791d
0xrin1 <0xrin1@protonmail.com>
2022-04-20 17:04:59 +0200

    correct interval on finalize deposit and redeem job files to one 60th of an hour

--- src/jobs/finalizeRedemptions.ts
+++ src/jobs/finalizeRedemptions.ts
@@ -2,7 +2,7 @@ import { finalizeRedemptions } from '../contracts/strategy';
 import { createJob } from './helpers';
 
 const JOB_NAME = 'finalizeRedemptions';
-const INTERVAL = 24;
+const INTERVAL = 1 / 60;
 
 export default createJob(JOB_NAME, INTERVAL, async function () {
   await finalizeRedemptions();
