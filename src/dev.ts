34f475f20f6015c38a24d4a59f2ccc71faa62a00
joaop21 <jpsilva9898@gmail.com>
2022-04-20 11:49:26 +0100

    Add dev entrypoint

---
+++ src/dev.ts
@@ -0,0 +1,44 @@
+import typeorm from 'fastify-typeorm-plugin';
+import { Connection } from 'typeorm';
+
+import * as API from './api';
+import getDBConnection from './db/getConnection';
+import * as Monitoring from './monitoring';
+import * as ServerScheduler from './server/scheduler';
+import * as WorkerScheduler from './worker/scheduler';
+
+let connection: Connection;
+
+Monitoring.start();
+
+getDBConnection().then(async (newConnection) => {
+  connection = newConnection;
+
+  API.server.register(typeorm, { connection: newConnection! });
+
+  await ServerScheduler.start();
+  await WorkerScheduler.start();
+  await API.start();
+});
+
+function handleExit(code?: number) {
+  console.log('closing');
+
+  Promise.all([
+    connection?.close(),
+    API.server.close(),
+    ServerScheduler.stop(),
+    WorkerScheduler.stop(),
+    Monitoring.stop(),
+  ]).finally(() => process.exit(code || 0));
+
+  setTimeout(() => process.abort(), 1000).unref();
+}
+
+process.on('SIGINT', handleExit);
+process.on('SIGQUIT', handleExit);
+process.on('SIGTERM', handleExit);
+process.on('uncaughtException', (err) => {
+  console.error('uncaught error', err);
+  handleExit(1);
+});
