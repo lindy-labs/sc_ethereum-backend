8322bc97d229e2ce226e04f797f9b788b073f437
joaop21 <jpsilva9898@gmail.com>
2022-04-26 19:38:51 +0100

    Register cors in server

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,28 @@
+import { assert } from 'console';
 import fastify from 'fastify';
+import cors from 'fastify-cors';
 
 import logger from '../logger';
 import { organizations } from '../organizations';
 
+assert(process.env.FRONTEND_URL);
+
 export const server = fastify({
   logger,
+});
+
+server.register(cors, {
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET'],
+  // allowedHeaders: Configures the Access-Control-Allow-Headers CORS header.
+  // exposedHeaders:  Configures the Access-Control-Expose-Headers CORS header.
+  // credentials: Configures the Access-Control-Allow-Credentials CORS header.
+  // maxAge: Configures the Access-Control-Max-Age CORS header.
+  // preflightContinue: Pass the CORS preflight response to the route handler.
+  // optionsSuccessStatus: Provides a status code to use for successful OPTIONS requests.
+  // preflight: You can entirely disable preflight by passing false here (default: true).
+  // strictPreflight: Enforces strict requirement of the CORS preflight request headers (Access-Control-Request-Method and Origin) as defined by the W3C CORS specification.
+  // hideOptionsRoute: hide options route from the documentation built using fastify-swagger (default: true).
 });
 
 server.get('/ping', async (_request, _reply) => {
