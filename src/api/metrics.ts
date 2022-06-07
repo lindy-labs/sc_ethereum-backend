fa4e8a90eb15f26d3ac89ee4921f0340b9ead317
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2022-06-07 11:41:10 +0100

    Add and endpoint to feed the foundation's performance chart (#34)
    The new endpoint returns a (value, timestamp) pair, where the value is the sum of the current total value of a foundation (shares * price per share) plus the amount claimed.

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -1,6 +1,11 @@
 import { FastifyInstance, RegisterOptions } from 'fastify';
+import { Decimal } from 'decimal.js';
 
-import { getVaultMetricRepository } from '../db';
+import {
+  getFoundationMetricRepository,
+  getVaultMetricRepository,
+  VaultMetric,
+} from '../db';
 
 const MAX_RECORDS = 1000;
 
@@ -25,6 +30,64 @@ export default function (
   _opts: RegisterOptions,
   done: Function,
 ) {
+  server.get<{ Params: { id: string } }>(
+    '/foundation/:id',
+    { schema },
+    async (request, reply) => {
+      const { start, end } = request.query as QueryArgs;
+
+      const repository = await getFoundationMetricRepository();
+
+      const rawData = await repository
+        .createQueryBuilder('foundation_metric')
+        .select([
+          'foundation_metric.shares as shares',
+          'foundation_metric.amountClaimed as "amountClaimed"',
+          'foundation_metric.amountDeposited as "amountDeposited"',
+          'v.value as performance',
+          'foundation_metric.createdAt as "createdAt"',
+        ])
+        .where('foundation_metric.id = :id', { id: request.params.id })
+        .where('foundation_metric.createdAt >= :start', { start })
+        .andWhere('foundation_metric.createdAt <= :end', { end })
+        .innerJoin('foundation_metric.vaultMetric', 'v')
+        .limit(MAX_RECORDS)
+        .getRawMany();
+
+      const data = rawData.map(
+        ({
+          shares,
+          amountClaimed,
+          amountDeposited,
+          performance,
+          createdAt,
+        }) => {
+          console.log({
+            shares,
+            amountClaimed,
+            amountDeposited,
+            performance,
+            createdAt,
+          });
+          return {
+            value: Decimal.max(
+              0,
+              new Decimal(shares)
+                .mul(performance)
+                .plus(amountClaimed)
+                .minus(amountDeposited),
+            ).toFixed(),
+            createdAt,
+          };
+        },
+      );
+
+      reply.header('Content-Type', 'application/json');
+      reply.status(200);
+      reply.send({ data });
+    },
+  );
+
   server.get('/day', { schema }, async (request, reply) => {
     const { start, end } = request.query as QueryArgs;
 
--- src/jobs/collectFoundationsPerformance.ts
+++ src/jobs/collectFoundationsPerformance.ts
@@ -1,8 +1,9 @@
 import { request, gql } from 'graphql-request';
 
+import { vaultPerformance } from '../contracts/vault';
 import config from '../config';
 import { createJob } from './helpers';
-import { getFoundationMetricRepository } from '../db';
+import { getConnection, VaultMetric, FoundationMetric } from '../db';
 
 const query = gql`
   {
@@ -30,20 +31,34 @@ const JOB_NAME = 'collectFoundationsPerformance';
 const INTERVAL = 12;
 
 export default createJob(JOB_NAME, INTERVAL, async function () {
-  const repository = await getFoundationMetricRepository();
-
   const { foundations }: Response = await request(config.graphURL, query);
 
-  await Promise.all(
-    foundations.map(async (foundation) => {
-      const metric = repository.create({
-        foundation: foundation.id,
-        amountClaimed: foundation.amountClaimed,
-        amountDeposited: foundation.amountDeposited,
-        shares: foundation.shares,
-      });
-
-      await repository.save(metric);
-    }),
+  const foundationMetricsRaw = await Promise.all(
+    foundations.map(async (foundation) => ({
+      foundation: foundation.id,
+      amountClaimed: foundation.amountClaimed,
+      amountDeposited: foundation.amountDeposited,
+      shares: foundation.shares,
+    })),
   );
+
+  const vaultMetricRaw = {
+    key: 'vault_performance',
+    value: await vaultPerformance(),
+  };
+
+  const connection = await getConnection();
+
+  await connection.transaction(async (transactionalEntityManager) => {
+    const vaultMetric = await transactionalEntityManager
+      .getRepository(VaultMetric)
+      .save(vaultMetricRaw);
+
+    await transactionalEntityManager.getRepository(FoundationMetric).save(
+      foundationMetricsRaw.map((foundation) => ({
+        ...foundation,
+        vaultMetric: vaultMetric,
+      })),
+    );
+  });
 });
