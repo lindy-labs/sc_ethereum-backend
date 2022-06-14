2d2b177be0ea483de0e8fc43e397605af280b994
joaop21 <jpsilva9898@gmail.com>
2022-06-14 22:47:49 +0100

    Keep file hash in memory

--- src/api/termsOfService.ts
+++ src/api/termsOfService.ts
@@ -1,4 +1,15 @@
 import { FastifyInstance, RegisterOptions } from 'fastify';
+import path from 'path';
+import fs from 'fs';
+import crypto from 'crypto';
+
+const hashFile = (filename: string) => {
+  const fileBuffer = fs.readFileSync(filename);
+  return crypto.createHash('sha256').update(fileBuffer).digest('hex');
+};
+
+const tosFile = path.join(__dirname, '../public/termsOfService.txt');
+const fileHash = hashFile(tosFile);
 
 export default function (
   server: FastifyInstance,
