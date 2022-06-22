8109a485c4da0bcede5561a626712854f364b6bd
0xrin <0xrin1@protonmail.com>
2022-06-22 19:27:46 +0200

    remove anchor related logic (#43)

--- src/jobs/finalizeRedemptions.ts
+++
@@ -1,9 +0,0 @@
-import { finalizeRedemptions } from '../contracts/strategy';
-import { createJob } from './helpers';
-
-const JOB_NAME = 'finalizeRedemptions';
-const INTERVAL = 1 / 60;
-
-export default createJob(JOB_NAME, INTERVAL, async function () {
-  await finalizeRedemptions();
-});
