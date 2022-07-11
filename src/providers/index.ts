f7436add02d54c66328d9211401fadf6195c841c
0xrin1 <0xrin1@protonmail.com>
2022-07-11 18:07:29 +0200

    change to JsonRPCProvider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,12 +1,12 @@
 import { providers, Wallet } from 'ethers';
 import config from '../config';
 
-export const provider: providers.WebSocketProvider =
-  new providers.WebSocketProvider(config.rpcURL.eth || 'ws://127.0.0.1:8545');
+export const provider: providers.JsonRpcProvider =
+  new providers.JsonRpcProvider(config.rpcURL.eth || 'http://127.0.0.1:8545');
 
-export const polygonProvider: providers.WebSocketProvider =
-  new providers.WebSocketProvider(
-    config.rpcURL.polygon || 'ws://127.0.0.1:8545',
+export const polygonProvider: providers.JsonRpcProvider =
+  new providers.JsonRpcProvider(
+    config.rpcURL.polygon || 'http://127.0.0.1:8545',
   );
 
 export const wallet: Wallet = Wallet.fromMnemonic(
